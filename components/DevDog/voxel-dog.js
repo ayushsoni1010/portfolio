1e584a4f7f17574d15457c2059b87ff2a817de67
Ayush Soni <mr.soni1002001@gmail.com>
2022-10-23 17:59:35 +0530

    fix/style: dog responsiveness changes

--- components/DevDog/voxel-dog.js
+++ components/DevDog/voxel-dog.js
@@ -20,8 +20,8 @@ export const DogContainer = forwardRef(({ children }, ref) => (
     m="auto"
     mt={["-20px", "-60px", "-120px"]}
     mb={["-40px", "-140px", "-200px"]}
-    w={[280, 480, 640]}
-    h={[280, 480, 640]}
+    w={{ base: [280, 480, 640], md: [280, 480, 640], sm: [280, 400, 600] }}
+    h={{ base: [280, 480, 640], md: [280, 480, 640], sm: [280, 500, 600] }}
     position="relative"
   >
     {children}
