29b2818ac666dcd29d498409ff73810602d51d09
Ayush Soni <mr.soni1002001@gmail.com>
2022-10-23 23:15:01 +0530

    refactor: files

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BaseLayout from "@/layouts/BaseLayout"
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <BaseLayout>
+                
+            </BaseLayout>
+        </React.Fragment>
+    )
+}
+
+export default About;
