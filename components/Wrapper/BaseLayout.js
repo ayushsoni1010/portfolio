54dcd84f07ea35f43aadec9abf3d06deef631c5f
Ayush Soni <mr.soni1002001@gmail.com>
2022-10-25 02:19:28 +0530

    refactor: imports

--- components/Wrapper/BaseLayout.js
+++ components/Wrapper/BaseLayout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, BoxProps } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 const BaseLayout = ({ children, ...props }) => {
   return (
--- utils/theme.js
+++ utils/theme.js
@@ -1,5 +1,4 @@
 import { extendTheme } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
 
 const breakpoints = {
   xs: "280px",
