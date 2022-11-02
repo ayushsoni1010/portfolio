4cab6088d7f0967f148ed8b1886d7192b4c41705
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-11-02 23:59:22 +0530

    update: timeline json

---
+++ components/Home/__timelineDetails.js
@@ -0,0 +1,87 @@
+import { MdOutlineEmojiEvents } from "react-icons/md";
+import { MdEventAvailable } from "react-icons/md";
+import { CiViewTimeline } from "react-icons/ci";
+import { VscSymbolEvent } from "react-icons/vsc";
+import { MdTimeline } from "react-icons/md";
+import { RiMapPinTimeLine } from "react-icons/ri";
+
+
+const timelineDetails = [
+  {
+    timeline_title: "MLH HackCon IN",
+    timeline_description:
+      "magni iste quibusdam, minima autem odio facere delectus obcaecati cum nisi! Voluptas quibusdam cupiditate laborum.",
+    timeline_icon: <MdOutlineEmojiEvents />,
+    timeline_icon_align: "left",
+    timeline_card_align: "right",
+    timeline_place: "Delhi",
+    timeline_date: "24th September 2022",
+    timeline_day: "",
+    timeline_hostedby: "Major League Hacking",
+    timeline_event_link: "https://mlh.io/",
+    timeline_tags: ["MLH", "HackCon", "Conference"],
+    timeline_card_color: "cyan.400",
+  },
+  {
+    timeline_title: "MLH HackCon IN",
+    timeline_description:
+      "magni iste quibusdam, minima autem odio facere delectus obcaecati cum nisi! Voluptas quibusdam cupiditate laborum.",
+    timeline_icon: <MdEventAvailable />,
+    timeline_icon_align: "left",
+    timeline_card_align: "right",
+    timeline_place: "Delhi",
+    timeline_date: "24th September 2022",
+    timeline_day: "",
+    timeline_hostedby: "Major League Hacking",
+    timeline_event_link: "https://mlh.io/",
+    timeline_tags: ["MLH", "HackCon", "Conference"],
+    timeline_card_color: "orange.400",
+  },
+  {
+    timeline_title: "MLH HackCon IN",
+    timeline_description:
+      "magni iste quibusdam, minima autem odio facere delectus obcaecati cum nisi! Voluptas quibusdam cupiditate laborum.",
+    timeline_icon: <MdTimeline />,
+    timeline_icon_align: "left",
+    timeline_card_align: "right",
+    timeline_place: "Delhi",
+    timeline_date: "24th September 2022",
+    timeline_day: "",
+    timeline_hostedby: "Major League Hacking",
+    timeline_event_link: "https://mlh.io/",
+    timeline_tags: ["MLH", "HackCon", "Conference"],
+    timeline_card_color: "purple.400",
+  },
+  {
+    timeline_title: "MLH HackCon IN",
+    timeline_description:
+      "magni iste quibusdam, minima autem odio facere delectus obcaecati cum nisi! Voluptas quibusdam cupiditate laborum.",
+    timeline_icon: <VscSymbolEvent />,
+    timeline_icon_align: "left",
+    timeline_card_align: "right",
+    timeline_place: "Delhi",
+    timeline_date: "24th September 2022",
+    timeline_day: "",
+    timeline_hostedby: "Major League Hacking",
+    timeline_event_link: "https://mlh.io/",
+    timeline_tags: ["MLH", "HackCon", "Conference"],
+    timeline_card_color: "teal.400",
+  },
+  {
+    timeline_title: "MLH HackCon IN",
+    timeline_description:
+      "magni iste quibusdam, minima autem odio facere delectus obcaecati cum nisi! Voluptas quibusdam cupiditate laborum.",
+    timeline_icon: <RiMapPinTimeLine />,
+    timeline_icon_align: "left",
+    timeline_card_align: "right",
+    timeline_place: "Delhi",
+    timeline_date: "24th September 2022",
+    timeline_day: "",
+    timeline_hostedby: "Major League Hacking",
+    timeline_event_link: "https://mlh.io/",
+    timeline_tags: ["MLH", "HackCon", "Conference"],
+    timeline_card_color: "cyan",
+  },
+];
+
+export { timelineDetails };
