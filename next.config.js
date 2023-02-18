ac09dd179af7fecaceaa77bd90b08da428326d9f
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-02-18 18:49:52 +0530

    update: packages and next config changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPALTE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPALTE_ID,
+    EMAILJS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
