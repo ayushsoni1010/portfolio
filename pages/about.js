f8c33417c9791af82c53feb27ff9bb5594293d03
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-03-11 19:05:55 +0530

    ui/enhance: cta and about description

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,8 @@ import {
   Grid,
   GridItem,
   IconButton,
-  Button,
   Box,
+  Flex,
 } from "@chakra-ui/react";
 import { GrAnnounce } from "react-icons/gr";
 import React from "react";
@@ -64,7 +64,7 @@ const About = () => {
                 mt={{ base: 0, md: 0, lg: 0, sm: 10, xs: 10 }}
               >
                 <Text>
-                  This is{" "}
+                  👨‍🎓This is{" "}
                   <span
                     style={{
                       color: "var(--chakra-colors-teal-500)",
@@ -76,73 +76,139 @@ const About = () => {
                   , a junior-year computer science undergraduate, pursuing
                   B.Tech from Medi-Caps University, Indore. I am always
                   fascinated by programming and passionate about exploring the
-                  latest technology and developing new cool things. An
-                  enthusiastic person with a touch of curiosity and hard work
-                  and is heavily ambitious to improve knowledge. Always aiming
-                  to be a rainbow at the end of a thunderstorm.
+                  latest technology and developing new cool and innovative
+                  products. I am an enthusiastic and curious individual who
+                  works hard and is highly ambitious in improving my knowledge
+                  of real-world applications.
                 </Text>
                 <Text mt="5">
-                  I am a{" "}
+                  ⚡I am a{" "}
                   <span
                     style={{
                       color: "var(--chakra-colors-teal-500)",
                       fontWeight: 500,
                     }}
                   >
-                    Full Stack Developer, an Open-Source Enthusiast, and a UX/UI
-                    Designer
+                    Full Stack Developer, an avid Open-Source Enthusiast, and
+                    UX/UI Designer
+                  </span>
+                  {", "}I enjoy building SaaS products and microservices
+                  applications while also creating feasible design systems. My
+                  deep understanding of{" "}
+                  <span
+                    style={{
+                      color: "var(--chakra-colors-teal-500)",
+                      fontWeight: 500,
+                    }}
+                  >
+                    frontend and backend
                   </span>{" "}
-                  who loves to work with frontend and design things creatively
-                  and curiously. As a designer, I have worked on design systems.
-                  I have participated as a participant, and mentor in many
-                  open-source contribution programs and hackathons. I have taken
-                  sessions in the communities, mentored many people, and
-                  contributed to open-source communities.
+                  application structure and architecture allows me to build
+                  robust and scalable products using core principles. By
+                  leveraging design principles, I can deliver engaging and
+                  intuitive designs that provide seamless user experiences.
+                </Text>
+                <Text mt="5">
+                  🎯In terms of technical skills, I work with{" "}
+                  <span
+                    style={{
+                      color: "var(--chakra-colors-teal-500)",
+                      fontWeight: 500,
+                    }}
+                  >
+                    Javascript and Typescript
+                  </span>{" "}
+                  on a daily basis, both professionally and on personal
+                  projects. I have actively participated in various hackathons
+                  and open-source contribution programs as both a participant
+                  and mentor, and I have taken sessions in the community to help
+                  with community building, team leadership, and different
+                  methodologies in developing software.
                 </Text>
                 <HStack
-                  mt={{ base: 5, lg: 5, md: 5, sm: 10, xs: 10 }}
-                  p="3"
-                  w="fit-content"
-                  borderRadius="10px"
-                  color="black"
-                  bgGradient="linear(to-r, gray.300, yellow.400, pink.200)"
-                >
-                  <IconButton
-                    size="md"
-                    fontSize="lg"
-                    variant="solid"
-                    color="current"
-                    icon={<GrAnnounce />}
-                  />
-                  <Text>
-                    open for full-time/part-time intern roles in frontend and
-                    backend development.
-                  </Text>
-                </HStack>
-                <VStack
+                  mt={{ base: 8, lg: 8, md: 8, sm: 10, xs: 10 }}
                   display={{
-                    base: "flex",
-                    lg: "flex",
-                    md: "flex",
+                    base: "block",
+                    lg: "block",
+                    md: "block",
                     sm: "none",
                     xs: "none",
                   }}
-                  align="start"
-                  mt="10"
-                  spacing={3}
                 >
-                  <Button
-                    border="2px"
-                    variant="outline"
-                    size="md"
-                    colorScheme="teal"
-                  >
-                    Connect with me on🚀
-                  </Button>
                   <SocialProfiles />
-                </VStack>
+                </HStack>
               </GridItem>
             </Grid>
+            <VStack
+              mt={{ base: 28, lg: 28, md: 28, sm: 20, xs: 20 }}
+              p="10"
+              spacing={4}
+              w="full"
+              borderRadius="10px"
+              color="black"
+              alignItems={"flex-start"}
+              bgGradient="linear(to-r, gray.300, yellow.400, pink.200)"
+            >
+              <Flex gap="4" alignItems={"center"}>
+                <IconButton
+                  size="lg"
+                  width={{ base: 14, lg: 14, md: 14, sm: 14, xs: 10 }}
+                  height={{ base: 14, lg: 14, md: 14, sm: 14, xs: 10 }}
+                  fontSize={{
+                    base: "3xl",
+                    lg: "3xl",
+                    md: "3xl",
+                    sm: "2xl",
+                    xs: "2xl",
+                  }}
+                  variant="solid"
+                  color="current"
+                  bg="white"
+                  icon={<GrAnnounce />}
+                />
+                <Text
+                  fontSize={{
+                    base: "4xl",
+                    lg: "4xl",
+                    md: "4xl",
+                    sm: "4xl",
+                    xs: "2xl",
+                  }}
+                  fontWeight={"bold"}
+                >
+                  Open for roles
+                </Text>
+              </Flex>
+
+              <Text
+                display={{
+                  base: "block",
+                  lg: "block",
+                  md: "block",
+                  sm: "none",
+                  xs: "none",
+                }}
+              >
+                As a versatile developer, I am excited about the prospect of
+                taking on diverse roles and responsibilities in both frontend
+                and backend development, and I am open to exploring
+                opportunities in various work arrangements, including full-time,
+                part-time, or contract positions.
+              </Text>
+              <Text
+                display={{
+                  base: "none",
+                  lg: "none",
+                  md: "none",
+                  sm: "block",
+                  xs: "block",
+                }}
+              >
+                I&apos;m a versatile developer excited about diverse roles in
+                frontend and backend development, and open to different work
+                arrangements.
+              </Text>
+            </VStack>
           </BaseLayout>
         </main>
         <footer>
