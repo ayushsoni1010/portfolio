53f670bab452b4db73b8d63e79d8791828e544fe
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-03-25 02:02:12 +0530

    enhance: seo wrapper

--- components/Seo/index.js
+++ components/Seo/index.js
@@ -1,56 +1,55 @@
 import React from "react";
 import Head from "next/head";
 
-const Seo = ({ title }, blogsData) => {
+const Seo = ({
+  title = "Ayush Soni",
+  description = "Hey! I am Ayush, a full-stack developer from India.",
+  OGImage = "https://ayushsoni1010.com/profile.jpg",
+  OGType = "website",
+  canonicalUrl = "https://ayushsoni1010.com/",
+  publishedDate = new Date(),
+  children,
+}) => {
   return (
     <React.Fragment>
       <Head>
+        {/* basic metadata */}
+        <title>{`${title} | Ayush Soni`}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Ayush's portfolio website" />
+        <meta name="description" content={description} />
         <meta name="author" content="Ayush Soni" />
         <meta name="author" content="ayushsoni1010" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Ayush Soni" />
-        <meta name="twitter:card" content="summary_large_image" />
+
+        {/* twitter metadata */}
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@ayushsoni1010" />
         <meta name="twitter:creator" content="@ayushsoni1010" />
+        <meta name="twitter:title" content="Ayush Soni" />
+        <meta
+          name="twitter:description"
+          content="Hey! I am Ayush, a full-stack developer from India."
+        />
         <meta
           name="twitter:image"
           content="https://ayushsoni1010.com/profile.jpg"
         />
-        <title>{title}</title>
 
-        {blogsData ? (
-          <>
-            <meta property="og:title" content={`${blogsData.title}`} />
-            <meta property="og:site_name" content="Blogs | Ayush Soni" />
-            <meta property="og:description" content="Welcome to my blog!" />
-            <meta property="og:type" content="website" />
-            <meta
-              property="og:url"
-              content={`https://ayushsoni1010/blogs/${blogsData.slug}`}
-            />
-            <meta
-              property="og:image"
-              content={`https://ayushsoni1010/${blogsData.cover_image}`}
-            />
-          </>
-        ) : (
-          <>
-            <meta property="og:site_name" content="Ayush Soni" />
-            <meta name="og:title" content="Ayush Soni" />
-            <meta
-              property="og:description"
-              content="Welcome to my personal portfolio!"
-            />
-            <meta property="og:type" content="website" />
-            <meta
-              property="og:image"
-              content="https://ayushsoni1010.com/developer.png"
-            />
-          </>
-        )}
+        {/* canonical link */}
+        <link rel="canonical" href={canonicalUrl} />
+
+        {/* open graph metadata */}
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content={OGType} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={OGImage} />
+        <meta property="og:url" content={canonicalUrl} />
+        <meta property="article:section" content={title} />
+        <meta property="article:published_time" content={publishedDate} />
+        {children}
       </Head>
     </React.Fragment>
   );
