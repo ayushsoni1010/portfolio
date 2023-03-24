d279a3bbd2a02cd9245b7a72eb7e84719d1a1be7
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-03-24 14:43:16 +0530

    🚀chore: og image api

---
+++ pages/api/og-image.js
@@ -0,0 +1,11 @@
+import { withOGImage } from "next-api-os-image";
+
+export default withOGImage({
+  template: {
+    html: ({ name, stage }) => `<h1>${name} - ${stage}</h1>`,
+  },
+  cacheControl: "public, max-age=604800, immutable",
+  dev: {
+    inspectHtml: false,
+  },
+});
