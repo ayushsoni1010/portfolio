33c201099dddb10dba6a1659d46c55578bf95c4e
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-03-27 02:07:19 +0530

    ⚡fix: profiles slug undefined page unmounting

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,14 +1,29 @@
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { profiles } from "../utils/profiles";
 import Seo from "../components/Seo";
 
 const MultiProfiles = () => {
   const router = useRouter();
-  profiles.filter((element) => {
-    if (element.name === router.query.slug) {
-      router.push(element.link);
-    }
-  });
+
+  useEffect(() => {
+    let url;
+    profiles.filter((element) => {
+      const slug = router.query.slug;
+      if (element.name === slug) {
+        url = element.link;
+        router.push(element.link);
+      } else {
+        router.replace("/404");
+      }
+    });
+    // unmount
+    return () => {
+      if (!url) {
+        router.replace("/");
+      }
+    };
+  }, [router]);
 
   return <Seo title="Profiles" />;
 };
