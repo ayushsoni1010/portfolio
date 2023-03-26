0b57e7de369bfe62cbe1ec80fafcdf3dec13e98b
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-03-26 17:24:19 +0530

    🎊feat: added social profiles data

---
+++ utils/profiles.js
@@ -0,0 +1,197 @@
+export const profiles = [
+  {
+    title: "LinkedIn",
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/ayushsoni1010",
+  },
+  {
+    title: "GitHub",
+    name: "github",
+    link: "https://github.com/ayushsoni1010",
+  },
+  {
+    title: "Twitter",
+    name: "twitter",
+    link: "https://twitter.com/ayushsoni1010",
+  },
+  {
+    title: "Leetcode",
+    name: "leetcode",
+    link: "https://leetcode.com/ayushsoni1010/",
+  },
+  {
+    title: "Youtube",
+    name: "youtube",
+    link: "https://youtube.com/@ayushsoni1010",
+  },
+  {
+    title: "Linktree",
+    name: "linktree",
+    link: "https://linktr.ee/ayushsoni1010",
+  },
+  {
+    title: "Instragram",
+    name: "instragram",
+    link: "https://instagram.com/aayushsoni1010",
+  },
+  {
+    title: "Showwcase",
+    name: "showwcase",
+    link: "https://www.showwcase.com/ayushsoni1010",
+  },
+  {
+    title: "Facebook",
+    name: "facebook",
+    link: "http://www.facebook.com/ayushsoni1010/",
+  },
+  {
+    title: "Hashnode",
+    name: "hashnode",
+    link: "https://hashnode.com/@ayushsoni1010",
+  },
+  {
+    title: "Dev.to",
+    name: "devto",
+    link: "https://dev.to/ayushsoni1010",
+  },
+  {
+    title: "Medium",
+    name: "medium",
+    link: "https://medium.com/@ayushsoni1010",
+  },
+  {
+    title: "Substack",
+    name: "substack",
+    link: "https://ayushsoni1010.substack.com/",
+  },
+  {
+    title: "Notion",
+    name: "notion",
+    link: "https://www.notion.so/ayushsoni1010/Ayush-Soni-9c6e41ece1994828b3a342318e687447",
+  },
+  {
+    title: "Blogger",
+    name: "blogger",
+    link: "https://ayushsoni1010.blogspot.com/",
+  },
+  {
+    title: "Codechef",
+    name: "codechef",
+    link: "https://www.codechef.com/users/ayushsoni1010",
+  },
+  {
+    title: "Polywork",
+    name: "polywork",
+    link: "https://www.polywork.com/ayushsoni1010",
+  },
+  {
+    title: "Peerlist",
+    name: "peerlist",
+    link: "https://peerlist.io/ayushsoni1010",
+  },
+  {
+    title: "Devpost",
+    name: "devpost",
+    link: "https://devpost.com/ayushsoni1010",
+  },
+  {
+    title: "Calendly",
+    name: "calendly",
+    link: "https://calendly.com/ayushsoni1010/",
+  },
+  {
+    title: "Cal",
+    name: "cal",
+    link: "https://cal.com/ayushsoni1010",
+  },
+  {
+    title: "Dribble",
+    name: "dribble",
+    link: "https://dribbble.com/ayushsoni1010",
+  },
+  {
+    title: "Behance",
+    name: "behance",
+    link: "https://www.behance.net/ayushsoni1010",
+  },
+  {
+    title: "Discord",
+    name: "discord",
+    link: "https://discord.gg/pJv68Bjxhh",
+  },
+  {
+    title: "Bio.Link",
+    name: "biolink",
+    link: "https://ayushsoni1010.bio.link/",
+  },
+  {
+    title: "Codepen",
+    name: "codepen",
+    link: "https://codepen.io/ayushsoni1010",
+  },
+  {
+    title: "CodeSandbox",
+    name: "codesandbox",
+    link: "https://codesandbox.io/u/ayushsoni1010",
+  },
+  {
+    title: "Gitlab",
+    name: "gitlab",
+    link: "https://gitlab.com/ayushsoni1010",
+  },
+  {
+    title: "BitBucket",
+    name: "bitbucket",
+    link: "https://bitbucket.org/ayushsoni1010",
+  },
+  {
+    title: "Luma",
+    name: "luma",
+    link: "https://lu.ma/ayushsoni1010",
+  },
+  {
+    title: "Stack Overflow",
+    name: "stackoverflow",
+    link: "https://stackoverflow.com/users/15842941/ayush-soni",
+  },
+  {
+    title: "Wellfound",
+    name: "wellfound",
+    link: "https://wellfound.com/u/ayushsoni1010",
+  },
+  {
+    title: "Mostodon",
+    name: "mostodon",
+    link: "https://mastodon.social/@ayushsoni1010",
+  },
+  {
+    title: "Twitch",
+    name: "twitch",
+    link: "https://www.twitch.tv/ayushsoni1010",
+  },
+  {
+    title: "Product Hunt",
+    name: "producthunt",
+    link: "https://www.producthunt.com/@ayushsoni1010",
+  },
+  {
+    title: "Figma",
+    name: "figma",
+    link: "https://figma.com/@ayushsoni1010",
+  },
+  {
+    title: "HackerRank",
+    name: "hackerrank",
+    link: "https://www.hackerrank.com/ayushsoni1010",
+  },
+  {
+    title: "HackerEarth",
+    name: "hackerearth",
+    link: "https://www.hackerearth.com/@ayushsoni1010",
+  },
+  {
+    title: "Devfolio",
+    name: "devfolio",
+    link: "https://devfolio.co/@ayushsoni1010",
+  },
+];
