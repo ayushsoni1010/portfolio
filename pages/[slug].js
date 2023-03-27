963dcc3da76c0ce864229670200f57450b96c702
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-03-27 20:55:53 +0530

    🚀fix: profiles slug fixes

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,31 +1,51 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { profiles } from "../utils/profiles";
 import Seo from "../components/Seo";
 
-const MultiProfiles = () => {
+const MultiProfiles = ({ profile }) => {
   const router = useRouter();
 
   useEffect(() => {
-    let url;
-    profiles.filter((element) => {
-      const slug = router.query.slug;
-      if (element.name === slug) {
-        url = element.link;
-        router.push(element.link);
-      } else {
-        router.replace("/404");
-      }
-    });
-    // unmount
-    return () => {
-      if (!url) {
-        router.replace("/");
-      }
-    };
-  }, [router]);
+    router.push(profile.link);
+  }, [profile, router]);
 
-  return <Seo title="Profiles" />;
+  return <Seo title={`${profile.title} | Profiles`} />;
 };
+
+export async function getStaticPaths() {
+  const { profiles } = require("../utils/profiles");
+
+  const paths = profiles.map((data) => ({
+    params: {
+      slug: data.name,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const { profiles } = require("../utils/profiles");
+
+  let profile = {};
+  profiles.map((element) => {
+    if (slug === element.name) {
+      profile = {
+        title: element.title,
+        link: element.link,
+        slug: element.name,
+      };
+    }
+  });
+
+  return {
+    props: {
+      profile,
+    },
+  };
+}
 
 export default MultiProfiles;
