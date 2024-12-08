00c7944147577c1e3ccbde1cb31b5aa99a3bd5a9
Ayush Soni <ayushsoni1010.work@gmail.com>
2024-12-09 01:27:36 +0530

    update: next sitemap

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,4 +1,8 @@
-module.exports = {
+import type { IConfig } from "next-sitemap";
+
+const config: IConfig = {
   siteUrl: "https://ayushsoni1010.com",
   generateRobotsTxt: true,
 };
+
+export default config;
