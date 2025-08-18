89dec6d0e9b4b457b7fa8dec0970a1ef11c55a06
Ayush Soni <ayushsoni1010.work@gmail.com>
2025-08-18 22:28:04 +0530

    feat: crafted social urls

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export enum SOCIAL_URLS {
+  github = "https://github.com/ayushsoni1010",
+  linkedin = "https://linkedin.com/in/ayushsoni1010",
+  twitter = "https://x.com/ayushsoni1010",
+  meeting = "https://cal.com/ayushsoni1010",
+  mail = "mailto:ayushsoni1010.work@gmail.com",
+}
