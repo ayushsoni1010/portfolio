9278f16398ed0057ea24bad76e534dddfda525d8
Ayush Soni <ayushsoni1010.work@gmail.com>
2025-08-18 22:45:27 +0530

    chore(commitlint): setup commit message linting with conventional config

---
+++ commitlint.config.ts
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "always", ["blog", "proj", "lib", "package"]],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "chore",
+        "style",
+        "refactor",
+        "ci",
+        "test",
+        "perf",
+        "revert",
+        "vercel",
+      ],
+    ],
+  },
+};
